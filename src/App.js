f12ef10e12d35ced2090d0cdd11200ee36f7958d
blake-lim <blake198492@kakao.com>
2023-01-12 15:35:06 +0900

    ADD: Nav바 완성

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from '././pages/Home'
+import './App.css'
+import Movies from '././pages/Movies'
+import MovieDetail from '././pages/MovieDetail'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <Navigation />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/movies' element={<Movies />} />
+        <Route path='/movies/:id' element={<MovieDetail />} />
+      </Routes>
     </div>
   );
 }
